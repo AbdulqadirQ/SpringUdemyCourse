c684e722a54157b92321b4442b449192c546918f
AbdulqadirQ <a.q.r@hotmail.co.uk>
2017-12-20 19:57:54 +0000

    Completion of tutorial 8. Bean Person given constructor arguments and tested

--- src/main/java/com/abdulqadirsprojects/springCourse/App.java
+++ src/main/java/com/abdulqadirsprojects/springCourse/App.java
@@ -10,14 +10,16 @@ public class App {
 		// ApplicationContext is a bean container and is a class that can instantiate beans
 		// This line creates a bean container. The the file which contains all beans for this project
 		// is located within the root directory, so /beans.xml
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/abdulqadirsprojects/springCourse/beans/beans.xml");
 		
 		// The bean attribute name in 'beans.xml' is called 'person'
 		// context is caste using the correct class, 'Person'
 		Person person = (Person)context.getBean("person");
 		person.speak();
 		
+		System.out.println(person);
 		
+		((FileSystemXmlApplicationContext)context).close();
 		
 		/* Usual way of creating a class object and using it:
 		 		
