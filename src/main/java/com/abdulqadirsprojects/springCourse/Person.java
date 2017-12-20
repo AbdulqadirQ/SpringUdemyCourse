7edac75cc129f7da6d0585c971d82cb6ae204d0b
AbdulqadirQ <a.q.r@hotmail.co.uk>
2017-12-20 20:11:09 +0000

    Completion of tutorial 9. Bean property created and demonstrated

--- src/main/java/com/abdulqadirsprojects/springCourse/Person.java
+++ src/main/java/com/abdulqadirsprojects/springCourse/Person.java
@@ -5,6 +5,8 @@ public class Person {
 	private int id;
 	private String name;
 	
+	private int taxId;
+	
 	public Person() {
 		
 	}
@@ -19,11 +21,16 @@ public class Person {
 		System.out.println("Hello I'm a person");
 	}
 
+	public void setTaxId(int taxId) {
+		this.taxId = taxId;
+	}
 
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
+		return "Person [id=" + id + ", name=" + name + ", taxId=" + taxId + "]";
 	}
+
+
 
 	
 	
