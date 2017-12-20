95f256ce1c4d03a6b70f87d289101cc50f6b0620
AbdulqadirQ <a.q.r@hotmail.co.uk>
2017-12-20 17:37:32 +0000

    first commit. Tutorial 5 of course - created bean from POJO class and injected for use

---
+++ src/main/java/com/abdulqadirsprojects/springCourse/App.java
@@ -0,0 +1,31 @@
+package com.abdulqadirsprojects.springCourse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		// ApplicationContext is a bean container and is a class that can instantiate beans
+		// This line creates a bean container. The the file which contains all beans for this project
+		// is located within the root directory, so /beans.xml
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		// The bean attribute name in 'beans.xml' is called 'person'
+		// context is caste using the correct class, 'Person'
+		Person person = (Person)context.getBean("person");
+		person.speak();
+		
+		
+		
+		/* Usual way of creating a class object and using it:
+		 		
+		Person person = new Person();
+		person.speak();
+		
+		*/
+
+	}
+
+}
---
+++ src/main/java/com/abdulqadirsprojects/springCourse/Person.java
@@ -0,0 +1,9 @@
+package com.abdulqadirsprojects.springCourse;
+
+public class Person {
+	
+	public void speak() {
+		System.out.println("Hello I'm a person");
+	}
+
+}
